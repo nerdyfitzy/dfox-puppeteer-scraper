e9e387a66a024583fd779ffb7958fa148c05d042
nerdyfitzy <william.fitzpatrick2003@gmail.com>
2025-04-28 11:54:07 -0400

    csv fixes for multiple opponents

--- csvmaker.js
+++ csvmaker.js
@@ -5,21 +5,19 @@ const file = JSON.parse(fs.readFileSync("./lessons.json"));
 let csv = "player,character,opponent,date,link,timestamped,notes\n";
 
 file.forEach((entry) => {
-  let d = new Date(entry.date);
-  let formatted = `${
-    d.getUTCMonth() + 1 < 10
-      ? `0${d.getUTCMonth() + 1}`
-      : `${d.getUTCMonth() + 1}`
-  }/${
-    d.getUTCDate() < 10 ? `0${d.getUTCDate()}` : `${d.getUTCDate()}`
-  }/${d.getFullYear()}`;
-  let entryString = `${entry.player},${entry.character},${entry.opponent},${formatted},${entry.link},${entry.timestamped},`;
-  if (entry.notes) {
-    entryString += `${entry.notes}\n`;
-  } else {
-    entryString += "\n";
-  }
-  csv += entryString;
+    let d = new Date(entry.date);
+    let formatted = `${d.getUTCMonth() + 1 < 10
+            ? `0${d.getUTCMonth() + 1}`
+            : `${d.getUTCMonth() + 1}`
+        }/${d.getUTCDate() < 10 ? `0${d.getUTCDate()}` : `${d.getUTCDate()}`
+        }/${d.getFullYear()}`;
+    let entryString = `${entry.player},${entry.character},"${entry.opponent}",${formatted},${entry.link},${entry.timestamped},`;
+    if (entry.notes) {
+        entryString += `${entry.notes}\n`;
+    } else {
+        entryString += "\n";
+    }
+    csv += entryString;
 });
 
 fs.writeFileSync("lessons.csv", csv);
